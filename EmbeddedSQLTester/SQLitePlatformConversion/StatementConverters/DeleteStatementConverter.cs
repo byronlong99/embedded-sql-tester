85e9c40202f6ce52b6ac5f79e11cdf40c3518905
Byron Long <byronlong@gmail.com>
2021-11-21 19:37:39 -0500

    adding support for deletes

---
+++ EmbeddedSQLTester/SQLitePlatformConversion/StatementConverters/DeleteStatementConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using EmbeddedSQLTester.SQLitePlatformConversion.StatementConverters.ClauseProcessors;
+using TSQL.Statements;
+
+namespace EmbeddedSQLTester.SQLitePlatformConversion.StatementConverters
+{
+    internal class DeleteStatementConverter
+    {
+        public List<ConverterBase> GetClauseProcessorList(TSQLDeleteStatement statement)
+        {
+            return new List<ConverterBase>
+            {
+                new GeneralClauseConverter(statement.Delete),
+                new FromClauseConverter(statement.From),
+                new WhereClauseConverter(statement.Where),
+            };
+        }
+    }
+}
